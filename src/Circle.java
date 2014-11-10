bc789b820e659f67bc7185494f23b352a1aa951f
dyla1920 <dyla1920@ISS147D8-14.scdsb.pri>
2014-11-10 09:19:36 -0500

    Lesson 6

--- src/Circle.java
+++ src/Circle.java
@@ -1,10 +1,56 @@
 
+import TurtleGraphics.Pen;
+
 public class Circle implements Shape {
 
-    
-    public double area() {
-        return 0;
+    private double xPos, yPos, radius;
+
+    public Circle() {
+        xPos = 0;
+        yPos = 0;
+        radius = 1;
     }
-    
-    
+
+    public Circle(double x, double y, double r) {
+        xPos = 0;
+        yPos = 0;
+        radius = 1;
+    }
+
+    public double area() {
+        return Math.PI*radius*radius;
+    }
+
+    public void draw(Pen p) {
+        p.up();
+        p.move(xPos, xPos);
+        p.down();
+        p.setDirection(0);
+    //    p.move(width);
+    }
+
+    public double getXPos() {
+        return xPos;
+    }
+
+    public double getYPos() {
+        return yPos;
+    }
+
+    public void move(double xLoc, double yLoc) {
+        xPos=xLoc;
+        yPos=yLoc;
+    }
+
+    public void stretchBy(double factor) {
+        radius *=factor;
+    }
+    public String toString(){
+        String str= "This is a CIRCLE\n";
+        str+="XLOC: "+xPos+ "\tYLOC: "+yPos+"\n";
+        str+="RADIUS: "+radius +"\n";
+        str+="AREA: "+ area();
+        return str;
+    }
+
 }
--- src/Rect.java
+++ src/Rect.java
@@ -1,9 +1,66 @@
 
-public class Rect implements Shape {
+import TurtleGraphics.Pen;
 
-    @Override
-    public double area(){
-    return 0;
-}
+public class Rect implements Shape {
+    
+    private double xPos, yPos, width, height;
+    
+    public Rect() {
+        xPos = 0;
+        yPos = 0;
+        width = 1;
+        height = 1;
+    }
+    
+    public Rect(double x, double y, double w, double h) {
+        xPos = x;
+        yPos = y;
+        width = w;
+        height = h;
+    }
+    
+    public double area() {
+        return width * height;
+    }
+    
+    public void draw(Pen p) {
+        p.up();
+        p.move(xPos, xPos);
+        p.down();
+        p.setDirection(0);
+        p.move(width);
+        p.turn(90);
+        p.move(height);
+        p.turn(90);
+        p.move(width);
+        p.turn(90);
+        p.move(height);
+    }
+    
+    public double getXPos() {
+        return xPos;
+    }
+    
+    public double getYPos() {
+        return yPos;
+    }
+    
+    public void move(double xLoc, double yLoc) {
+        xPos = xLoc;
+        yPos = yLoc;
+    }
+    
+    public void stretchBy(double factor) {
+        width *= factor;
+        height *= factor;
+    }
+    
+    public String toString() {
+        String str = "This is a Rectangle\n";
+        str += "XLOC: " + xPos + "\tYLOC: " + yPos + "\n";
+        str += "WIDTH: " + width + "\tHEIGHT: " + height + "\n";
+        str += "AREA: " + area();
+        return str;
+    }
     
 }

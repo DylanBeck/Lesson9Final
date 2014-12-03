18b72dce49731722a90f3ec1bc797a4967623284
dyla1920 <dyla1920@ISS171D8-T2.scdsb.pri>
2014-12-03 11:53:12 -0500

    Emp

--- src/PartTimeEmployee.java
+++ src/PartTimeEmployee.java
@@ -2,11 +2,12 @@
 public class PartTimeEmployee extends Employee {
    
     public PartTimeEmployee(){
-        super();
-        LOW_HOURS=1;
-    }
+        super();}
+        
+    
     public double getPay(){
       //  totalPay +=LOW_HOURS*LOW_RATE;
-        return (LOW_HOURS*LOW_RATE);
+        totalpay += rate*hours;
+        return (rate * hours);
     }
 }
